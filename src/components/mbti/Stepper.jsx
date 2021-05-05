60aa1db69a80e454e7d0d7d2c0c4329303eac0a8
leye195 <leye195@naver.com>
2021-05-05 23:58:31 +0900

    feat: add Stepper components

--- src/components/mbti/Stepper.jsx
+++ src/components/mbti/Stepper.jsx
@@ -1,5 +1,7 @@
 import { css } from "@emotion/react";
 import PropTypes from "prop-types";
+import { flexMixin } from "styles/_mixin";
+import { Colors } from "styles/_variables";
 
 const stepperStyle = css`
   margin: auto auto 1rem auto;
@@ -7,9 +9,32 @@ const stepperStyle = css`
   height: 2rem;
   position: relative;
 `;
-const lineStyle = css``;
+const lineStyle = css`
+  margin: 1rem 0;
+  width: 100%;
+  height: 3px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  border-radius: 2px;
+  background-color: ${Colors.purple};
+  z-index: 1;
+`;
 const circleStyle = css`
+  ${flexMixin({
+    direction: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  })}
+  height: 2rem;
+  width: 2rem;
+  font-weight: 600;
+  font-size: 1.2rem;
+  position: absolute;
+  color: white;
   border-radius: 50%;
+  background-color: ${Colors.mattpurple};
+  z-index: 2;
 `;
 
 const Stepper = ({ step = 1 }) => {
